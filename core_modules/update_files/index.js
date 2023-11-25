2dde09b02c6993f02bab65bca733aab29b58fd8e
Matheus <matheusgarciadg@gmail.com>
2023-11-25 18:40:35 -0300

    updating files with fs.appendFile

---
+++ core_modules/update_files/index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    const urlInfo = url.parse(req.url, true)
+    const name = urlInfo.query.name
+
+    if(!name) {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+            return res.end()
+        })
+    } else {
+        const nameNewLine = name + "\r\n"
+        fs.appendFile('file.txt', nameNewLine, (err, data) => {
+            res.writeHead(302, {
+                location: '/',
+            })
+            return res.end()
+        })
+    }
+})
+
+server.listen(port, () => console.log(`list port ${port}`))
